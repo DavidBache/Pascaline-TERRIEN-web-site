8dd1da7a742d42594aec124a684c8aee1d429f26
DavidBache <davidbache@orange.fr>
2026-02-12 11:05:17 +0100

    feat: ajout du système d'actualités avec Decap CMS

---
+++ src/routes/cms/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    onMount(() => {
+        // Redirige vers le fichier HTML statique
+        window.location.replace('/cms/index.html');
+    });
+</script>
+
+<svelte:head>
+    <title>Redirection vers CMS...</title>
+</svelte:head>
+
+<div class="flex items-center justify-center min-h-screen">
+    <p>Chargement du CMS...</p>
+</div>
